0962dca30f5b5b85da87a8f657d92616024ef8c4
kshitij583415 <97234504+kshitij583415@users.noreply.github.com>
2024-01-05 20:16:16 +0530

    Update upload.component.ts

--- src/app/lab-analysis/upload/upload.component.ts
+++ src/app/lab-analysis/upload/upload.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit, NgModule, enableProdMode } from '@angular/core';
+import { Component, enableProdMode } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { HttpClient } from '@angular/common/http';
 import * as XLSX from 'xlsx';
-import { VirtualTimeScheduler } from 'rxjs';
-import { DxChartModule } from 'devextreme-angular';
-import { BrowserModule } from '@angular/platform-browser';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import Chart, { BubbleController } from 'chart.js/auto'
+import Chart from 'chart.js/auto';
 
 if (!/localhost/.test(document.location.host)) {
   enableProdMode();
@@ -17,51 +14,70 @@ if (!/localhost/.test(document.location.host)) {
   styleUrls: ['./upload.component.css'],
 })
 export class UploadComponent {
-  ExcelData: any;
+  excelData: any;
   x: Text[] = [];
   y: number[] = [];
 
   willDownload = false;
+  showToaster: boolean = false;
+  toasterMessage: string = '';
 
-  customizeText(arg: any) {
-    return `GRADE ${arg.valueText}`;
-  }
-
-  constructor() {}
+  constructor(private http: HttpClient, private toastr: ToastrService) {}
 
   ngOnInit(): void {}
-  ReadExcel(event: any) {
-    let file = event.target.files[0];
 
+  readExcel(event: any) {
+    let file = event.target.files[0];
     let fileReader = new FileReader();
     fileReader.readAsBinaryString(file);
+
     fileReader.onload = (e) => {
       var workBook = XLSX.read(fileReader.result, { type: 'binary' });
       var sheetNames = workBook.SheetNames;
-      this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
-      console.log(this.ExcelData);
+      this.excelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
+
       var i = 0;
-      for (let data of this.ExcelData) {
+      for (let data of this.excelData) {
         this.x[i] = data.GRADE;
         this.y[i++] = data.COUNT;
       }
-      // console.log(this.y)
-      var src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"
-      var myChart = new Chart("myChart", {
-        type: "bar",
+
+      var myChart = new Chart('myChart', {
+        type: 'bar',
         data: {
-          labels : this.x,
-          datasets: [{
-            label:'Number of students',
-            data: this.y,
-            backgroundColor:['#68aeff'],
-            borderWidth:1
-          }]
+          labels: this.x,
+          datasets: [
+            {
+              label: 'Number of students',
+              data: this.y,
+              backgroundColor: ['#68aeff'],
+              borderWidth: 1,
+            },
+          ],
         },
         options: {
-          responsive:true
-        }
+          responsive: true,
+        },
       });
     };
   }
-}
+
+  submitFile() {
+    if (this.excelData && this.excelData.length > 0) {
+      // Assuming you want to simulate a delay before showing the toaster (you can remove this setTimeout)
+      setTimeout(() => {
+        this.showToaster = true;
+        // this.toasterMessage = 'File uploaded successfully!';
+        this.toastr.success('File uploaded successfully!');
+      }, 1000); // Adjust the delay time as needed
+  
+      // You can also perform additional actions with the uploaded data if required
+      console.log('Uploaded Data:', this.excelData);
+    } else {
+      // No file selected, show error toaster
+      this.showToaster = true;
+      // this.toasterMessage = 'Error uploading file. Please select a file.';
+      this.toastr.error('Error uploading file. Please select a file.');
+    }
+  }
+}  
